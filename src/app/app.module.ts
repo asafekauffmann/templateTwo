a1431eaf500c5516ca036f9d213f1582ddd66881
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-12-08 15:59:02 -0300

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @NgModule({
    declarations: [
@@ -14,7 +16,9 @@ import { TableComponent } from './table/table.component';
    imports: [
       BrowserModule,
       AppRoutingModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule,
+      PaginationModule.forRoot()
    ],
    providers: [],
    bootstrap: [AppComponent]
