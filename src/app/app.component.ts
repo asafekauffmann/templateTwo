ffea3dd966f77f948db41ed1565ffee523a866f8
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-12-08 14:36:38 -0300

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,59 @@ import { Component } from '@angular/core';
    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-   dados: Array<{ id: number, nome: string }> = [];
+   dados: Array<{ id: number, paciente: string, recepcao: string }> = [];
 
    constructor() {
       this.dados = [
          {
-            "id": 442,
-            "nome": "Murtle"
+            "id": 1,
+            "paciente": "Martin Luther",
+            "recepcao": "12/06/2021"
          },
          {
-            "id": 443,
-            "nome": "George"
+            "id": 2,
+            "paciente": "Lenny Love",
+            "recepcao": "17/05/2020"
+         },
+         {
+            "id": 3,
+            "paciente": "Rose Defa",
+            "recepcao": "11/02/2020"
+         },
+         {
+            "id": 4,
+            "paciente": "Dant Roy",
+            "recepcao": "21/06/2019"
+         },
+         {
+            "id": 5,
+            "paciente": "Naty Mad",
+            "recepcao": "11/11/2017"
+         },
+         {
+            "id": 6,
+            "paciente": "Alle James",
+            "recepcao": "11/07/2015"
+         },
+         {
+            "id": 7,
+            "paciente": "Ana Ribas",
+            "recepcao": "11/06/2012"
+         },
+         {
+            "id": 8,
+            "paciente": "Lenny Scard",
+            "recepcao": "11/03/2011"
+         },
+         {
+            "id": 9,
+            "paciente": "Rob Dirdeck",
+            "recepcao": "11/10/2016"
+         },
+         {
+            "id": 10,
+            "paciente": "Paty Maned",
+            "recepcao": "11/09/2019"
          }
       ];
    };
